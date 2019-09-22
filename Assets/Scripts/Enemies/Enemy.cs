7a15203663e4b06bc1490c72340e6ec1785af0e5
josh <48034855+JDJoffe@users.noreply.github.com>
2019-09-22 17:14:10 +1000

    enemy move and remove dumb terrains

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //public List<Transform> enemyWaypoints;
+    public Transform wayPoint1;
+    public Transform wayPoint2;
+    public bool reachedDestination;
+    public GameObject enemy;
+    private void Start()
+    {
+        enemy = this.gameObject;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (reachedDestination == true)
+        {
+           enemy.transform.position = Vector3.MoveTowards(enemy.transform.position, wayPoint2.position, .1f);
+        }
+        if (reachedDestination == false)
+        {
+           enemy.transform.position = Vector3.MoveTowards(enemy.transform.position, wayPoint1.position, .1f);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == ("WayPoint1"))
+        {
+            reachedDestination = true;
+        }
+        if (other.tag == ("WayPoint2"))
+        {
+            reachedDestination = false;
+        }
+    }
+
+}
